7cca89812f2ac91025d4014cf378f11c52fa64b5
Yooseungha <seungha06@nate.com>
2022-07-15 17:40:16 +0900

    Test Uploading

---
+++ gugu.py
@@ -0,0 +1,11 @@
+
+# sys.stdout = open("./Test.txt",'w')
+f = open("./Test.txt", "w")
+for i in range(2,10):
+    for x in range(1,10):
+        if i == 8:
+            continue
+        lines = (str(i),"X",str(x),"=", str(i*x),"\n")
+        f.writelines(lines)
+f.close()
+# sys.stdout.close()
