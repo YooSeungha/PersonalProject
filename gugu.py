659b12395dc1f0a4314d60c08cc3f3eed3a8698e
Yooseungha <seungha06@nate.com>
2022-07-18 08:43:32 +0900

    (최종)수정본

--- gugu.py
+++ gugu.py
@@ -1,11 +1,14 @@
+# import sys
 
-# sys.stdout = open("./Test.txt",'w')
-f = open("./Test.txt", "w")
+# sys.stdout = open('./Test.txt','w',encoding='utf-8')
+f = open("./Test.txt", "w", encoding='utf-8')
 for i in range(2,10):
     for x in range(1,10):
         if i == 8:
             continue
-        lines = (str(i),"X",str(x),"=", str(i*x),"\n")
-        f.writelines(lines)
+        else:
+            lines = (str(i),"X",str(x),"=", str(i*x),"\n")
+            f.writelines(lines)
+            # f.write("{} x {} = {}\n".format(i,x,i*x))
 f.close()
 # sys.stdout.close()
