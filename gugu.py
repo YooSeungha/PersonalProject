05a142006e4547b6c3f47ba976df72fcdc0f2f92
YooSeungha <90462118+YooSeungha@users.noreply.github.com>
2022-07-18 08:54:50 +0900

    Update gugu.py
    "(최종) 업데이트"

--- gugu.py
+++ gugu.py
@@ -1,5 +1,6 @@
+# import sys
 
-
+# sys.stdout = open('./gugu.txt','w',encoding='utf-8')
 f = open("./Test.txt", "w")
 for i in range(2,10):
     for x in range(1,10):
@@ -8,3 +9,4 @@ for i in range(2,10):
         lines = (str(i),"X",str(x),"=", str(i*x),"\n")
         f.writelines(lines)
 f.close()
+# f.sys.stdout.close()
